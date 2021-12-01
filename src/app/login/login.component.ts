af6e95847d23a8b9fa04280d73fb07cbbbcbe04e
ghassenbns <ghassenbenslimane00@gmail.com>
2021-12-01 14:36:44 +0100

    Setting Routing + generating login + register components

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserAuthService } from '../shared/user-auth.service';
+import { User } from '../shared/user.model';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  constructor(private router: Router, private userService: UserAuthService) {}
+
+  ngOnInit() {}
+  onRegister() {
+    this.router.navigate(['register']);
+  }
+  onLogin(loginForm: NgForm) {
+    if (!loginForm.valid) {
+      const user = new User(loginForm.value.email, loginForm.value.password);
+      this.userService
+        .login(user)
+        .subscribe((response) => console.log(response));
+    }
+  }
+}
