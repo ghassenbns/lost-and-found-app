115b68f67e13ebc107d818bb2fb66582b3f075ff
ghassenbns <ghassenbenslimane00@gmail.com>
2021-12-01 11:50:01 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lost-and-found',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
