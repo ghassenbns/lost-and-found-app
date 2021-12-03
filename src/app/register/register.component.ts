8d92b4337a64a4b93e884df99eff6fad8b12511c
ghassenbns <ghassenbenslimane00@gmail.com>
2021-12-03 17:39:43 +0100

    Error Handling in Register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ErrorService } from '../shared/error.service';
+import { ToastService } from '../shared/toast.service';
 import { UserAuthService } from '../shared/user-auth.service';
 import { UserRegister } from '../shared/user.model';
 
@@ -10,7 +12,12 @@ import { UserRegister } from '../shared/user.model';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  constructor(private router: Router, private userService: UserAuthService) {}
+  constructor(
+    private router: Router,
+    private userService: UserAuthService,
+    private toastService: ToastService,
+    private errorService: ErrorService
+  ) {}
 
   ngOnInit() {}
   onRegister(registerForm: NgForm) {
@@ -24,13 +31,26 @@ export class RegisterComponent implements OnInit {
         registerForm.value.username
       );
       this.userService.register(user).subscribe(
-        (response) => console.log(response),
-        (error) => {
-          console.log(error);
+        () => this.toastService.openToast('Utilisateur crée', 'success'),
+        () => {
+          this.toastService.openToast('Email existe déja', 'danger');
         },
         () => {
           this.router.navigate(['login']);
         }
+      );
+    } else if (
+      registerForm.valid &&
+      registerForm.value.password !== registerForm.value.verif
+    ) {
+      this.toastService.openToast(
+        'Les mots de passe ne sont pas identiques',
+        'danger'
+      );
+    } else {
+      this.toastService.openToast(
+        'Veillez remplir les champs avec des valeurs valides',
+        'danger'
       );
     }
   }
